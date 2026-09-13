c2547fd3e0f90a73c88d520df8e8a6af613094ea
NovaAltair <rishithapuralasetti@gmail.com>
2026-09-13 20:09:50 +0530

    added code

--- Shoppingcart.c
+++ Shoppingcart.c
@@ -2,21 +2,24 @@
 int main()
 {
     char item[100];
-    char currency = '₹';
+    char currency;
     float price=0.0;
     int quantity=0;
     float total=0.0;
 
     printf ("welcome to vintage valley's shopping cart\n");
     printf ("what item would you like to purchase? ");
-    printf("I would like to purchase item: %c\n", item);
     fgets(item, 100, stdin);
+
+    printf("I would like to purchase item: %s\n", item);
+    
     printf ("how much does the item cost?");
     scanf ("%f", &price);
     printf ("the item cost is:%.3f\n",price);
     printf ("how many items would you like to purchase?");
     scanf ("%d", &quantity);
     total = price * quantity;
+    printf("You have prurchased %d %s at a cost of %.3f each\n", quantity, item, price);
     printf ("the total cost is:%.3f\n",total);
     
 
